73055f9713267264d1dda8e569975a8dc6142e38
Shoaib Ullah Rasel <shoaibullahrasel@gmail.com>
2023-06-19 15:06:31 +0600

    learn something new

--- js/event.js
+++ js/event.js
@@ -1,1 +1,21 @@
-console.log("This is separate JS file");
+//option 1: directly set on the html element
+
+//option 2: add onclick function
+function makepink(){
+    document.body.style.background = 'pink';
+}
+//option 3: 
+//another way to use
+const makeBlueButton = document.getElementById('make-blue');
+
+makeBlueButton.onclick= makeBlue;
+
+function makeBlue(){
+    document.body.style.background = 'blue';
+}
+//option 3 another
+const yellowButton = document.getElementById("make-yellow");
+
+yellowButton.onclick = function yellowButton() {
+    document.body.style.backgroundColor = "yellow";
+}
