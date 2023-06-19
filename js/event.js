8778bac6f15742afbcbda5452db07afdf86e1c5b
Shoaib Ullah Rasel <shoaibullahrasel@gmail.com>
2023-06-19 15:46:12 +0600

    add more things

--- js/event.js
+++ js/event.js
@@ -1,6 +1,8 @@
 //option 1: directly set on the html element
+//<button onclick="console.log(65)">Another button</button>
 
 //option 2: add onclick function
+//use this one for our project
 function makepink(){
     document.body.style.background = 'pink';
 }
@@ -14,8 +16,27 @@ function makeBlue(){
     document.body.style.background = 'blue';
 }
 //option 3 another
-const yellowButton = document.getElementById("make-yellow");
+    const yellowButton = document.getElementById("make-yellow");
+     yellowButton.onclick = function yellowButton() {
+     document.body.style.backgroundColor = "yellow";
+    }
 
-yellowButton.onclick = function yellowButton() {
-    document.body.style.backgroundColor = "yellow";
+        const cyanButton = document.getElementById('make-cyan');
+        cyanButton.addEventListener('click', makecyan)
+
+       function makecyan(){
+        document.body.style.backgroundColor = 'cyan';
 }
+//option 4: another
+const makeGreenButton = document.getElementById('make-green');
+       makeGreenButton.addEventListener('click', function makeGreen(){
+        document.body.style.backgroundColor = 'green';})
+
+//option 4 final
+ 
+    //document.getElementById('make-goldenrod').addEventListener('click', function() {})
+    /*very very importand we will use it sometime */
+        document.getElementById('make-goldenrod').addEventListener('click', function(){
+        document.body.style.backgroundColor = 'goldenrod';
+       
+    })
